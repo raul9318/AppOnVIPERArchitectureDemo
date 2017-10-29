59f76dc6bf25368d6fa17550fdc21890e4d266ca
Рамиль Ибрагимов <r.ibragimov.do@mail.ru>
2017-10-29 17:46:06 +0300

    Prepare for create friend's details module

---
+++ SimpleTestOfAppOnVIPER/Modules/Details of friend/User Interface/View/FriendDetailsViewInterface.swift
@@ -0,0 +1,13 @@
+//
+//  FriendDetailsViewInterface.swift
+//  SimpleTestOfAppOnVIPER
+//
+//  Created by Рамиль Ибрагимов on 29.10.2017.
+//  Copyright © 2017 Рамиль Ибрагимов. All rights reserved.
+//
+
+import Foundation
+
+protocol FriendDetailsViewInterface: class {
+    func displayData(name: String, age: Int)
+}
